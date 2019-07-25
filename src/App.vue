50d39d9c354c4e5b222181197875e6870db52729
fkj <dashen_mr.f@aliyun.com>
2019-07-25 11:51:32 +0800

    v1.0.2

--- src/App.vue
+++ src/App.vue
@@ -7,45 +7,45 @@
     ></clock>
     <Block
     left-title='历年统计数据左一'
-    block-height='285px'
+    block-height='305px'
     block-width='400px'
     block-left='15px'
-    block-top='-150px'
+    block-top='-170px'
     ></Block>
     <Block
     left-title='历年统计数据左二'
     block-height='285px'
     block-width='400px'
     block-left='15px'
-    block-top='-120px'
+    block-top='-140px'
     ></Block>
     <Block
     left-title='历年统计数据中一'
     block-height='385px'
     block-width='620px'
     block-left='445px'
-    block-top='-705px'
+    block-top='-725px'
     ></Block>
     <Block
     left-title='历年统计数据中二'
     block-height='170px'
     block-width='620px'
     block-left='445px'
-    block-top='-675px'
+    block-top='-695px'
     ></Block>
     <Block
     left-title='历年统计数据'
     block-height='185px'
     block-width='400px'
     block-left='1095px'
-    block-top='-1210px'
+    block-top='-1230px'
     ></Block>
     <Block
     left-title='历年统计数据右二'
     block-height='305px'
     block-width='400px'
     block-left='1095px'
-    block-top='-1180px'
+    block-top='-1200px'
     ></Block>
   </div>
 </template>
@@ -73,6 +73,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 750px;
   margin-top: 10px;
 }
 #app > #clock{
